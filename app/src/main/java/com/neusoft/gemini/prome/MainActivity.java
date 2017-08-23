5ce398c8fbeb89fe8c012a0d36753051de379a4c
1051353960 <1051353960@qq.com>
2017-08-23 19:11:02 +0800

    陶毅

--- app/src/main/java/com/neusoft/gemini/prome/MainActivity.java
+++ app/src/main/java/com/neusoft/gemini/prome/MainActivity.java
@@ -9,6 +9,6 @@ private int i;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+ sssssssssssssssssssssssssssssssssssssssssss
     }
 }
